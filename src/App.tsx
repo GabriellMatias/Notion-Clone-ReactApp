5acb6a44eb885cab714964e34ae09739736918bb
GabriellMatias <gabriel.goiasat@gmail.com>
2023-07-12 23:38:53 -0300

    :hammer: Feat: set bubbleButtons propierties

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Editor } from './Components/Editor'
 
 function App() {
   return (
-    <div className="min-h-screen text-zinc-900 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 p-8">
-      <div className="bg-white w-[1100px] mx-auto rounded-xl overflow-hidden grid grid-cols-[16rem_1fr] min-h-[720px] shadow-sm border border-black/20">
-        <aside className="bg-zinc-50 border-r-zinc-100 p-4">
+    <div className="min-h-screen text-zinc-50 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 p-8">
+      <div className="bg-zinc-800 w-[1100px] mx-auto rounded-xl overflow-hidden grid grid-cols-[16rem_1fr] min-h-[720px] shadow-sm border border-black/20">
+        <aside className="bg-zinc-900 border-r-zinc-700 p-4">
           <div className="flex gap-2 group">
             <button className="w-3 h-3 rounded-full bg-zinc-300 group-hover:bg-red-500" />
             <button className="w-3 h-3 rounded-full bg-zinc-300 group-hover:bg-yellow-500" />
--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,4 +1,4 @@
-import { useEditor, EditorContent } from '@tiptap/react'
+import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { InitialContent } from './InitialContent'
 import 'highlight.js/styles/atom-one-dark.css'
@@ -7,6 +7,16 @@ import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import { lowlight } from 'lowlight'
 import ts from 'highlight.js/lib/languages/typescript'
 import html from 'highlight.js/lib/languages/xml'
+
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+  RxChevronDown,
+  RxChatBubble,
+} from 'react-icons/rx'
+import { BubbleButton } from './BubbleButton'
 
 lowlight.registerLanguage('html', html)
 lowlight.registerLanguage('ts', ts)
@@ -27,9 +37,40 @@ export function Editor() {
     },
   })
   return (
-    <EditorContent
-      editor={editor}
-      className="max-w-[700px] mx-auto pt-16 prose "
-    />
+    <>
+      <EditorContent
+        editor={editor}
+        className="max-w-[700px] mx-auto pt-16 prose prose-invert "
+      />
+      {editor && (
+        <BubbleMenu
+          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg flex overflow-hidden divide-x divide-zinc-600"
+          editor={editor}
+        >
+          <BubbleButton>
+            <RxChevronDown className="w-4 h-4" />
+            Text
+          </BubbleButton>
+          <BubbleButton>
+            <RxChatBubble className="w-4 h-4" />
+            Comment
+          </BubbleButton>
+          <div className="items-center flex">
+            <BubbleButton>
+              <RxFontBold className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton>
+              <RxFontItalic className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton>
+              <RxStrikethrough className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton>
+              <RxCode className="w-4 h-4" />
+            </BubbleButton>
+          </div>
+        </BubbleMenu>
+      )}
+    </>
   )
 }
